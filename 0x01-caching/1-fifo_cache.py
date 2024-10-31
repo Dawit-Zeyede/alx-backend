63c595fe4a51f32849d29cd3e7437b2d22b4913f
Dawit-Zeyede <devazeyede10@gmail.com>
2024-10-31 08:55:00 -0700

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''
+1-fifo_cache.py.
+'''
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    inherits from BaseCaching and is a caching system.
+    '''
+    def __init__(self):
+        '''
+        Initializes the cache.
+        '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''
+        Adds an item in the cache.
+        '''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        '''
+        Retrieves an item by key.
+        '''
+        return self.cache_data.get(key, None)
